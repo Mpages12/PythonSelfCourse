11b9cff94ed8bff19f04edd91df4d8b3214ed16a
Mpages12 <98711167+Mpages12@users.noreply.github.com>
2022-02-21 13:41:11 -0800

    Add files via upload

---
+++ helloworld/strings.py
@@ -0,0 +1,11 @@
+print("Today is a good day to learn python")
+print("Python is fun")
+print("Strings are easy to use")
+print("we can even include 'quotes' in string")
+print("hello" + " world")
+greeting = "hello"
+name = input("Please enter your name ")
+print (greeting + name)
+
+# if we want a space, we can add that too
+print(greeting + " " + name)

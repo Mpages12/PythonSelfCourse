5452ce388737a49df01a32cff7bc4bb9c97aaa38
Mpages12 <98711167+Mpages12@users.noreply.github.com>
2022-02-22 15:55:32 -0800

    Lesson Negatives and Strings

--- helloworld/strings2.py
+++ helloworld/strings2.py
@@ -3,19 +3,8 @@
 parrot = "Norwegian Blue" 
 
 print(parrot[0:6]) #Norweg last charcte is not included
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
+print(parrot[-14:-8])
 
-print(parrot[10:14])
-print(parrot[10:])
+print(parrot[-4:-2]) #Bl
+print(parrot[-4:12]) #Bl
 
-print(parrot[:6])
-print(parrot[6:])
- 
-print(parrot[:6] + parrot[6:])
- 
-print(parrot[:])
-
-letters = "abcdefghijklmnopqrstuvwxyz"
- 

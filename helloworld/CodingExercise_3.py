56885365b248bd27f2a8609d31332eabb7cb65e9
Mpages12 <mpages12@aol.com>
2022-02-21 20:15:16 -0800

    course test

--- helloworld/CodingExercise_3.py
+++ helloworld/CodingExercise_3.py
@@ -1,3 +1,8 @@
 print("Number 1\The Larch")
 print("Number 2\The Horse Chestnut")
 print("hello")
+
+
+bun_price = 2.40
+money = 15
+print( money // bun_price)
--- helloworld/strings.py
+++ helloworld/strings.py
@@ -4,8 +4,19 @@ print("Strings are easy to use")
 print("we can even include 'quotes' in string")
 print("hello" + " world")
 greeting = "hello"
-name = input("Please enter your name ")
+name = "Tim"
 print (greeting + name)
 
 # if we want a space, we can add that too
 print(greeting + " " + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + " is " + age + " years old")
+print(type(age))

87136217058b2a23af0a88625fa1f44a8a5ec3e6
Mpages12 <mpages12@aol.com>
2022-02-21 20:30:34 -0800

    Class Example

--- helloworld/operators.py
+++ helloworld/operators.py
@@ -10,12 +10,17 @@ print(a % b ) # 0 modulo: the reminader after integer division
 
 print()
 
-for i in range(1, a // b):
-    print(i)
-    
-i = 1
-print(i)
-i = 2
-print(i)
-i = 3
-print (i)
+print(a + b / 3 - 4 * 12)
+print(a + (b/3) - (4 * 12))
+print((((a + b) /3) -4) * 12)
+print(((a+b) /3 -4) *12)
+
+c = a + b 
+d = c /3
+e = d - 4
+print (e*12)
+
+print()
+
+print(a / (b *a) /b)
+ 

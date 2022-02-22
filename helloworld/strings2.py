ae80859691430414fe91fb5384a2b1e0f2769b94
Mpages12 <98711167+Mpages12@users.noreply.github.com>
2022-02-22 15:11:02 -0800

    Slicing Stings Lecture

--- helloworld/strings2.py
+++ helloworld/strings2.py
@@ -2,19 +2,20 @@
 #         012345678901234
 parrot = "Norwegian Blue" 
 
-print(parrot)
+print(parrot[0:6]) #Norweg last charcte is not included
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
 
-print(parrot[3]) 
-print(parrot[4])
-print()
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+print(parrot[10:14])
+print(parrot[10:])
 
-print()
-print(parrot[-11]) 
-print(parrot[-10])
-print()
-print(parrot[-11])
-print(parrot[6-14])
-print(parrot[8-14])
+print(parrot[:6])
+print(parrot[6:])
+ 
+print(parrot[:6] + parrot[6:])
+ 
+print(parrot[:])
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+ 

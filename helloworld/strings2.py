fcf92d8fc0325dd6dabdcca90e814d892ad8cad5
Mpages12 <98711167+Mpages12@users.noreply.github.com>
2022-02-22 16:49:09 -0800

    slice and steps lesson

--- helloworld/strings2.py
+++ helloworld/strings2.py
@@ -2,9 +2,12 @@
 #         012345678901234
 parrot = "Norwegian Blue" 
 
-print(parrot[0:6]) #Norweg last charcte is not included
-print(parrot[-14:-8])
+print(parrot[0:6:2]) #Nre
+print(parrot[0:6:3]) #Nw
 
-print(parrot[-4:-2]) #Bl
-print(parrot[-4:12]) #Bl
+number ="9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
 
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
